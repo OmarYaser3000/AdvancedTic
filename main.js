f2d73c9e52e35ed0d4ca0c913ec46be4fe8e21fa
OmarYaser <expertomar3000@gmail.com>
2023-09-26 17:03:17 +0200

    initialized game variables and the pieces on the board to start adding functionality
    - edited a mistake in the flowchart
    - styled the text in the boxes

--- main.js
+++ main.js
@@ -30,3 +30,35 @@ const initializeBoard = () => {
 };
 
 initializeBoard();
+
+// ###############
+// main game play
+// ###############
+
+const boxes = document.querySelectorAll(".box");
+
+// initialize the variables
+let board = Array.from(
+  { length: settings.boardSize ** 2 },
+  (_, index) => index
+);
+const playerOne = "X";
+const playerTwo = "O";
+let playerOneMovedPieces = 0;
+let playerTwoMovedPieces = 0;
+let currentPlayer = playerOne;
+
+// initialize the players on the board
+let numberOfPieces = settings.boardSize;
+for (let i = 0; i < numberOfPieces; i++) {
+  boxes[i].innerHTML = playerTwo;
+  board[i] = playerTwo;
+  boxes[boxes.length - i - 1].innerHTML = playerOne;
+  board[boxes.length - i - 1] = playerOne;
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("box") && e.target.innerHTML === "") {
+    console.log(e.target.getAttribute("name"));
+  }
+});
