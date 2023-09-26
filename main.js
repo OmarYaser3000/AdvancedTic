c0c8bcc2d5fe54dd91d0280e9884cca9fd090c13
OmarYaser <expertomar3000@gmail.com>
2023-09-26 15:13:21 +0200

    initialized the board and made it dynamic according to game settings
    - added Noto Sans font from google fonts
    - added font awesome library

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 import "./css/style.css";
 
-document.querySelector("#app").innerHTML = ``;
+// game settings
+let settings = {
+  boardSize: 3,
+};
+
+// create the html structure
+document.querySelector("#app").innerHTML = `
+  <div class="container">
+    <div class="board">
+    </div>
+  </div>
+`;
+
+// board initialization
+const initializeBoard = () => {
+  const board = document.querySelector(".board");
+  const root = document.documentElement;
+
+  root.style.setProperty("--board-col", settings.boardSize);
+  root.style.setProperty("--length", `${settings.boardSize * 100}px`);
+
+  for (let i = 0; i < settings.boardSize ** 2; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.setAttribute("name", i);
+    board.appendChild(box);
+  }
+};
+
+initializeBoard();
