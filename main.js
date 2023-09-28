0d11cdfe6869cf6416389a27a1f9d4da18d4dff6
OmarYaser <expertomar3000@gmail.com>
2023-09-28 17:46:55 +0200

    the player can move his pieces and the app tracks that he moved all of his pieces

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ let settings = {
   boardSize: 3,
 };
 
+// allowed moves
+const generateAllowedMoves = (boardSize) => {
+  const allowed = {};
+
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    const neighbors = [];
+
+    // Check left neighbor
+    if (i % boardSize !== 0) {
+      neighbors.push(i - 1);
+    }
+
+    // Check right neighbor
+    if (i % boardSize !== boardSize - 1) {
+      neighbors.push(i + 1);
+    }
+
+    // Check top neighbor
+    if (i >= boardSize) {
+      neighbors.push(i - boardSize);
+    }
+
+    // Check bottom neighbor
+    if (i < boardSize * (boardSize - 1)) {
+      neighbors.push(i + boardSize);
+    }
+
+    allowed[i] = neighbors;
+  }
+
+  return allowed;
+};
+
 // create the html structure
 document.querySelector("#app").innerHTML = `
   <div class="container">
@@ -42,11 +75,13 @@ let board = Array.from(
   { length: settings.boardSize ** 2 },
   (_, index) => index
 );
+const allowedMoves = generateAllowedMoves(settings.boardSize);
 const playerOne = "X";
 const playerTwo = "O";
-let playerOneMovedPieces = 0;
-let playerTwoMovedPieces = 0;
+let playerOneMovedPieces = [];
+let playerTwoMovedPieces = [];
 let currentPlayer = playerOne;
+let oldBox;
 
 // initialize the players on the board
 let numberOfPieces = settings.boardSize;
@@ -58,7 +93,44 @@ for (let i = 0; i < numberOfPieces; i++) {
 }
 
 document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("box") && e.target.innerHTML === "") {
-    console.log(e.target.getAttribute("name"));
+  // const clickedBox = parseInt(e.target.getAttribute("name"));
+
+  if (e.target.classList.contains("box") && e.target.innerHTML === playerOne) {
+    oldBox = parseInt(e.target.getAttribute("name"));
+    // remove the highlights from all boxes
+    boxes.forEach((item) => {
+      item.classList.remove("highlighted");
+    });
+    // highlight the available moves to the selected piece
+    let allowedList = allowedMoves[parseInt(e.target.getAttribute("name"))];
+    for (let i = 0; i < allowedList.length; i++) {
+      if (boxes[allowedList[i]].innerHTML === "") {
+        boxes[allowedList[i]].classList.add("highlighted");
+      }
+    }
+  }
+
+  if (
+    e.target.classList.contains("box") &&
+    e.target.innerHTML === "" &&
+    e.target.classList.contains("highlighted")
+  ) {
+    e.target.innerHTML = playerOne;
+    board[parseInt(e.target.getAttribute("name"))] = playerOne;
+    board[oldBox] = oldBox;
+    boxes[oldBox].innerHTML = "";
+    playerOneMovedPieces.push(oldBox);
+    if (
+      playerOneMovedPieces.includes(6) &&
+      playerOneMovedPieces.includes(7) &&
+      playerOneMovedPieces.includes(8)
+    ) {
+      // checkForWin()
+      console.log("winnable");
+    }
+    // remove the highlights from all boxes
+    boxes.forEach((item) => {
+      item.classList.remove("highlighted");
+    });
   }
 });
